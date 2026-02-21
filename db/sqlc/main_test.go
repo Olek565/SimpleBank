75c302b833bbf511ef3e98c795b15ffff221a3d2
Mihnea <mihnea.varzaru@gmail.com>
2026-02-21 20:01:07 +0200

    fixing main_test for env error

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,40 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Olek565/SimpleBank/util"
 	_ "github.com/lib/pq"
 )
+
+// var testQueries *Queries
+// var testDB *sql.DB
+
+// func TestMain(m *testing.M) {
+// 	config, err := util.LoadConfig("../..")
+// 	if err != nil {
+// 		log.Fatal("Cannot load config: ", err)
+// 	}
+
+// 	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+// 	if err != nil {
+// 		log.Fatal("Cannot connect to db:", err)
+// 	}
+
+// 	testQueries = New(testDB)
+
+// 	os.Exit(m.Run())
+// }
 
 var testQueries *Queries
 var testDB *sql.DB
 
-func TestMain(m *testing.M) {
-	config, err := util.LoadConfig("../..")
-	if err != nil {
-		log.Fatal("Cannot load config: ", err)
-	}
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
+)
 
-	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+func TestMain(m *testing.M) {
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
